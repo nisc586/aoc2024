fb688a3ad38a1fd142a6b76df400e3eb6e74511c
nisc586 <49026680+nisc586@users.noreply.github.com>
2024-12-02 02:35:01 +0100

    Create Gradle Project

---
+++ src/main/java/com/nisc586/App.java
@@ -0,0 +1,7 @@
+package com.nisc586;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/nisc586/AppTest.java
@@ -0,0 +1,11 @@
+package com.nisc586;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    
+    @Test
+    public void verifyNoExceptionThrown() {
+        App.main(new String[]{});
+    }
+}
