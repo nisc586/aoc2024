f694d91d905c066d525b6555edc25186a8869b3e
nisc586 <49026680+nisc586@users.noreply.github.com>
2025-01-15 12:34:02 +0100

    Solve Day 7 Part 2

--- src/main/java/com/nisc586/Day07.java
+++ src/main/java/com/nisc586/Day07.java
@@ -1,5 +1,6 @@
 package com.nisc586;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -21,7 +22,17 @@ public class Day07 {
     }
 
     public static String part2(String input) {
-        return "";
+        List<List<Long>> equations = parse(input);
+
+        BigInteger total = BigInteger.ZERO;
+        for(List<Long> equation : equations) {
+            Long testVal = equation.getFirst();
+            List<Long> nums = equation.subList(2, equation.size());
+            Long x=equation.get(1);
+            if (checkEquation2(x, testVal, nums)) total = total.add(BigInteger.valueOf(testVal));
+            }
+        
+        return total.toString();
     }
 
     public static boolean checkEquation(Long x, Long testValue, List<Long> rest) {
@@ -38,6 +49,24 @@ public class Day07 {
         }
     }
 
+    public static boolean checkEquation2(Long x, Long testValue, List<Long> rest) {
+        if (rest.isEmpty()) {
+            return x.equals(testValue);
+        } else if (x.compareTo(testValue) > 0) {
+            // Not all numbers were consumed, but x is already greater than testValue
+            return false;
+        } else {
+            // Make recursive calls
+            boolean multiplyResult = checkEquation2(x * rest.getFirst(), testValue, rest.subList(1, rest.size()));
+            boolean addResult = checkEquation2(x + rest.getFirst(), testValue, rest.subList(1, rest.size()));
+            boolean concatResult = checkEquation2(concat(x, rest.getFirst()), testValue, rest.subList(1, rest.size()));
+            return multiplyResult || addResult || concatResult;
+        }
+    }
+
+    public static long concat(long x, long y) {
+        return Long.valueOf(String.valueOf(x) + String.valueOf(y));
+    }
 
     private static List<List<Long>> parse(String input) {
         List<List<Long>> result = new ArrayList<>();
@@ -56,4 +85,5 @@ public class Day07 {
         lineScanner.close();
         return result;
     }
+
 }
--- src/test/java/com/nisc586/Day07Test.java
+++ src/test/java/com/nisc586/Day07Test.java
@@ -15,9 +15,31 @@ public class Day07Test {
         assertEquals("3749", Day07.part1(input));
     }
 
+
+    @Test
+    public void testPart2() {
+        String input = Utils.getInputText(filePath);
+        assertEquals("11387", Day07.part2(input));
+    }
+
     @Test
     public void testCheckEquationLastNumIsZero() {
         List<Long> nums = List.of(19L, 1L);
         assertEquals(true, Day07.checkEquation(10L, 190L, nums));
+    }
+
+    @Test
+    public void testCheckEquation2ConcatAllNums() {
+        List<Long> nums = List.of(1L,1L,1L);
+        assertEquals(true, Day07.checkEquation2(1L, 1111L, nums));
+    }
+
+    @Test
+    public void testConcatOperation() {
+        assertEquals(1234L, Day07.concat(12L, 34L));
+        assertEquals(1230L, Day07.concat(123L, 0L));
+        assertEquals(123L, Day07.concat(0L, 123L));
+        assertEquals(11L, Day07.concat(1L, 1L));
+        assertEquals(0L, Day07.concat(0L, 0L));
     }
 }
