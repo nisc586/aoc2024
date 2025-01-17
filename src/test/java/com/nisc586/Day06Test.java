ac15a06338923076019cf5c6262e08291ce8ba23
nisc586 <49026680+nisc586@users.noreply.github.com>
2025-01-17 17:04:49 +0100

    Try reflection on a test class

--- src/test/java/com/nisc586/Day06Test.java
+++ src/test/java/com/nisc586/Day06Test.java
@@ -1,21 +1,61 @@
 package com.nisc586;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
 public class Day06Test {
-    private int dayNumber = 6;
-    private String filePath = String.format("src\\test\\resources\\day%1$02d.txt", dayNumber);
+
+    public static int dayNumber = 6;
+    public static String filePath = String.format("src\\test\\resources\\day%1$02d.txt", dayNumber);
+    public static String className = String.format("com.nisc586.Day%1$02d", dayNumber);
+    public static Class<?> dayClass;
+    public static Method part1, part2;
+
+    @BeforeAll
+    static void initAll() {
+        try {
+            dayClass = Class.forName(className);
+            part1 = dayClass.getMethod("part1", String.class);
+            part2 = dayClass.getMethod("part2", String.class);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class" + className + "not found.");
+        } catch (NoSuchMethodException ee){
+            System.out.println("Method not found in class" + className + ".");
+        }
+
+    }
+
+
     @Test
     public void testPart1() {
         String input = Utils.getInputText(filePath);
-        assertEquals("41", Day06.part1(input));
+        try {
+            assertEquals("41", part1.invoke(null, input));
+        } catch (InvocationTargetException e) {
+            fail("Method throws Exception.\n" + e.getMessage());
+        } catch (IllegalAccessException e) {
+            fail("Method throws Exception.\n" + e.getMessage());
+
+        }
     }
 
     @Test
     public void testPart2() {
         String input = Utils.getInputText(filePath);
-        assertEquals("6", Day06.part2(input));
+        try {
+            assertEquals("6", part2.invoke(null, input));
+        } catch (InvocationTargetException e) {
+            fail("Method throws Exception.\n" + e.getMessage());
+        } catch (IllegalAccessException e) {
+            fail("Method throws Exception.\n" + e.getMessage());
+
+        }
     }
 }
